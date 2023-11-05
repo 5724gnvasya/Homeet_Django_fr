e46636a47a6352d3b48b446d14606690dababbda
Alla Chernova <chall2005@yandex.ru>
2023-11-05 20:12:24 +0300

    Initial commit

---
+++ profil/views.py
@@ -0,0 +1,23 @@
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, redirect
+# from djangouploads.models import Person
+
+# Create your views here.
+# def home(request):
+#     return HttpResponse("ok")
+#
+# def person(request, person_id):
+#     person = Person.objects.get(pk=person_id)
+#     if person is not None:
+#         return render(request,'people/person.html', {'person': person})
+#     else:
+#         raise Http404('Person does not exist')
+def index(request):
+    context = {'title': 'Homeet',}
+    return render(request, 'profil/index.html', context)
+
+def profil(request):
+    context = {'title': 'Homeet - Profile'}
+    return render(request, 'profil/profil.html', context)
+def upload(request):
+    return render(request,'people/upload.html')
